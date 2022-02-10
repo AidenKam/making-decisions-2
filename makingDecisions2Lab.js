4de0ec25f8e2871f0a7177decb5ac5d7fabf93cb
Aiden Kampman <aidenshanekampman@gmail.com>
2022-02-10 11:15:03 -0700

    Problem 8 Complete

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -116,6 +116,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+listOfNumbers.forEach(function(index){
+  if (index%3 === 0){
+    console.log(`${index} is divisible by 3`)
+  }else console.log(index)
+})
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
